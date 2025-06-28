810229cd5301db5d313d86cb60a53830a53263a0
gagal18 <gagaleskibojan@gmail.com>
2025-06-28 21:30:35 +0200

    feat: Change context dynammicly112

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Pomo Start ! [demo change]';
+    return 'Pomo Start ! [demo change111]';
   }
 }
