a69b9634b600f87e6f736e44cffab85fbc163b1c
gagal18 <gagaleskibojan@gmail.com>
2025-06-28 21:22:37 +0200

    demo: Test workflows

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Pomo Start';
+    return 'Pomo Start ! [demo change]';
   }
 }
